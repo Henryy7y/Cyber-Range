ed5b0d271cfe4d2efc9fb30cefcaf4db83563dde
Henryy <leehern77@gmail.com>
2024-09-30 10:45:21 +0800

    registered admin

--- scoreboard/admin.py
+++ scoreboard/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import SMBExercise
+from .models import SMBExercise, ExInfo, ExLog
 
 admin.site.register(SMBExercise)
+admin.site.register(ExInfo)
+admin.site.register(ExLog)
 
